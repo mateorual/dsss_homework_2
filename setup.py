08f429f5b23b7127b5733c4fca3d33e29bd872b7
Mateo Ruiz Alvarez <mateo.a.ruiz@fau.de>
2023-11-13 23:04:07 +0100

    Adjustment to 'setup.py' regarding the style

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
+from setuptools import find_packages
 
 setup(
    name='math_quiz',
    version='1.0.0',
-   scripts=['math_quiz.py', 'tests_math_quiz.py'],
    description='A module to generate some random arithmetic operations, ask for an answer to the user and give a feedback',
    author='Mateo Ruiz Alvarez',
    author_email='mateo.a.ruiz@fau.de',
